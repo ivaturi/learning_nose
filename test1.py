be8671624d5bf66898d7518cd535312a566b82d3
abhi <abhi@mailbox.org>
2016-09-25 10:01:45 +0200

    some notes on the basic test

--- test1.py
+++ test1.py
@@ -1,9 +1,13 @@
 # basic testing
+# nose looks for 'test_' methods, and runs them
 def test_func():
     assert 'b' == 'b'
 
 
 # a slightly more complicated example:
+#
+# In this case, nose will create an instance of TestExample,
+# and only then run the test_c function
 class TestExample:
     def test_c(self):
         assert 'c' == 'c'
