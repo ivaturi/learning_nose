53d633945ef74357cb11167a343bbeb0d3b015d4
abhi <abhi@mailbox.org>
2016-09-25 10:17:48 +0200

    basics of unit testing

--- test1.py
+++ test1.py
@@ -21,5 +21,42 @@ class TestExample:
         assert 'c' == 'c'
 
 """
+Basics of unit testing
+-----------------------------------
+
 This is a common test pattern:
+
+def test():
+    setup_test()
+    try:
+        do_test()
+        make_test_assertions()
+    finally:
+        cleanup_after_test()
+
+
+setup_test():
+    Creates necessary objects, sets up database connections, find files,etc.
+    (basically, sets up everything that is required to run the test)
+
+
+do_test(), make_test_assertions():
+    Actually run the test
+
+cleanup_test_code():
+    Regardless of status of the test, cleans up or tears down the preconditions
+
+
+Fixtures
+---------
+
+Usually, frameworks provide for setting and tearing down preconditions,like so:
+
+test.setUp = set_up_test
+test.tearDown = cleanup_after_test
+
+def test():
+    do_test()
+    make_test_assertions()
+
 """
