cfe7ea80d3c935d3e1d9f21064a977fdf45ae894
abhi <abhi@mailbox.org>
2016-09-26 07:36:56 +0200

    fibonacci test cases for 0,1,2,3

--- tests/test_mathtools.py
+++ tests/test_mathtools.py
@@ -19,3 +19,7 @@ def test_fibonacci():
     assert fibonacci(5)  == [0,1,1,2,3]
     assert fibonacci(7)  == [0,1,1,2,3,5,8]
     assert fibonacci(10) == [0,1,1,2,3,5,8,13,21,34]
+    assert fibonacci(0)  == []
+    assert fibonacci(1)  == [0]
+    assert fibonacci(2)  == [0,1]
+    assert fibonacci(3)  == [0,1,1]
